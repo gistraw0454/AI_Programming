57ea6bae90577c601388315a80406c2cc9c608c2
gistraw0454 <nastraw0454@naver.com>
2024-11-21 16:04:17 +0900

    HW08

---
+++ HW08/optimizer.py
@@ -0,0 +1,241 @@
+from setup import Setup
+import random
+
+class Optimizer(Setup):
+    def __init__(self):
+        Setup.__init__(self)
+
+class MetaHeuristic(Optimizer):
+    def __init__(self):
+        Optimizer.__init__(self)
+
+class SimulatedAnnealing(MetaHeuristic):
+    def __init__(self):
+        MetaHeuristic.__init__(self)
+    def setVariables(self,_):
+        pass
+
+class  HillClimbing(Optimizer):
+    def __init__(self):
+        # 1. Setup에 정의된 delta, alpha, dx에 접근하기 위해 Setup 초기화
+        # 2. self._pType 정의하기 (Tsp인지, Numeric인지 구분하기 위한 Integer 변수 선언)
+        # 3. self._limitStuck 정의하기 (지금은 First-choice에서만 사용하지만, 앞으로 추가될
+        #    다른 hillclimbing 알고리즘에서 사용함)
+        Optimizer.__init__(self)
+        self._pType = 0
+        self._limitStuck = 100
+        self._numRestart = 1
+
+    def setVariables(self, pType):
+        # 1. pType을 인자로 받아서 self._pType에 assign
+        self._pType = pType
+
+    def displaySetting(self):
+        # 1. pType==1 (Numeric) 일 때만, Mutation step size를 출력하는 함수
+        # first-choice.py 코드의 ‘print(＂Mutation step size:＂, p.getDelta())’ 부분 활용
+        if self._pType == 1:
+            print()
+            print("Mutation step size:", self._delta)
+
+    def run(self):
+        pass
+
+    def randomRestart(self, p):         
+        i = 10
+        self.run(p)
+        bestSolution = p.getSolution()
+        bestMinimum = p.getValue()
+        numEval = p.getNumEval()
+
+        while i < self._numRestart:
+            self.run(p)
+            i += 1
+
+            newSolution = p.getSolution()
+            newMinimum = p.getValue()
+            numEval += p.getNumEval()
+
+            if newMinimum < bestMinimum:
+                bestSolution = newSolution
+                bestMinimum = newMinimum
+
+
+        p.storeResult(bestSolution, bestMinimum)
+
+class Stochastic(HillClimbing):
+    def __init__(self):
+        HillClimbing.__init__(self)
+
+    def displaySetting(self):
+        print()
+        print("Search Algorithm: Stochastic Hill Climbing")
+        print()
+        HillClimbing.displaySetting(self)
+
+    def run(self, p):
+        current = p.randomInit()  
+        valueC = p.evaluate(current)
+        i = 0
+
+        while i < self._limitStuck:
+            neighbors = p.mutants(current)
+            temp, valueS = self.stochasticBest(neighbors, p)
+
+            if valueS < valueC:
+                current = temp
+                valueC = valueS
+                i = 0 
+
+            else:
+                i += 1
+
+        p.storeResult(current, valueC)
+
+    def stochasticBest(self, neighbors, p):
+        valuesMin = [p.evaluate(indiv) for indiv in neighbors]
+        large = max(valuesMin) + 1
+        valuesMax = [large - val for val in valuesMin]
+
+        total = sum(valuesMax)
+        randValue = random.uniform(0, total)
+        s = valuesMax[0]
+
+        for i in range(len(valuesMax)):
+            if randValue <= s: 
+                break
+            else:
+                s += valuesMax[i+1]
+
+        nextSolution=neighbors[i]
+        nextValue=valuesMin[i]
+        return nextSolution, nextValue
+
+    
+
+    
+
+class FirstChoice(HillClimbing):
+    def displaySetting(self):
+        print()
+        print("Search algorithm: First-Choice Hill Climbing")
+        super().displaySetting()    # HillClimbing.displaySetting(self) 도 가능
+        print("Max evaluations with no improvement: {0:,} iterations".format(self._limitStuck))
+    
+    def run(self,p):
+        current = p.randomInit()    # 랜덤한 current 결정
+        valueC = p.evaluate(current)    # 현재의 evaluation 계산
+        i = 0
+        while i < self._limitStuck:  # limit 만큼 돌면서 새 solution을 찾을 때까지 계속 돌아준다.
+            successor = p.randomMutant(current) # randMutant로 랜덤하게 변형하여 solution을 만든다.
+            valueS = p.evaluate(successor)  # 그 솔루션의 evalution을 계산한다.
+            if valueS < valueC: # new solution이 더 좋으면 바꿔준다. <- 반복
+                current = successor
+                valueC = valueS
+                i = 0
+            else:
+                i += 1
+        p.storeResult(current, valueC)
+
+
+class SteepestAscent(HillClimbing):
+    def displaySetting(self):
+        print()
+        print("Search algorithm: Steepest-Ascent Hill Climbing")
+        super().displaySetting()
+
+    def run(self,p):
+        current = p.randomInit() # 'current' is a list of values
+        valueC = p.evaluate(current)
+        while True:
+            neighbors = p.mutants(current)
+            successor, valueS = self.bestOf(neighbors,p)    # self.
+            if valueS >= valueC:
+                break
+            else:
+                current = successor
+                valueC = valueS
+        p.storeResult(current,valueC)
+    
+    def bestOf(self,neighbors, p): # self, 추가
+        best = neighbors[0]
+        bestValue = p.evaluate(best)
+        
+        for neighbor in neighbors[1:]:
+            value = p.evaluate(neighbor)
+            if value < bestValue:
+                best = neighbor
+                bestValue = value
+        return best, bestValue
+
+class GradientDescent(HillClimbing):
+    def displaySetting(self):
+        print()
+        print("Search algorithm: Gradient Descent")
+        print()
+        print("Udate rate:", self._alpha)
+        print("Increment for calculating derivative:", self._dx)
+    
+    def run(self,p):
+        currentP = p.randomInit()  # Current point
+        valueC = p.evaluate(currentP)
+        while True:
+            nextP = p.takeStep(currentP, valueC)
+            valueN = p.evaluate(nextP)
+            if valueN >= valueC:
+                break
+            else:
+                currentP = nextP
+                valueC = valueN
+        p.storeResult(currentP, valueC)
+
+class SimulatedAnnealing(MetaHeuristic):
+    def __init__(self):
+        self._numSample=100
+        self._limitEval=100000
+        self._whenBestFound=0
+
+    def displaySetting(self):
+        print("Search Algorithm: Simulated Annealing")
+        print("Number of evaluations until termination: {0:,}".format(self._limitEval))
+    
+    def run(self, p):
+        current = p.randomInit()
+        valueC = p.evaluate(current)
+        best, valueBest = current, valueC
+        whenBestFound = i=1
+        t = self.initTemp(p) 
+        while True:
+            ... # Implement
+        self._whenBestFound=whenBestFound
+        p.storeResult(best, valueBest)
+    
+    def stochasticBest(self, neighbors, p):
+        # Smaller valuse are better in the following list
+        valuesForMin = [p.evaluate(indiv) for indiv in neighbors]
+        largeValue = max(valuesForMin) + 1
+        valuesForMax = [largeValue - val for val in valuesForMin]
+        # Now, larger values are better
+        total = sum(valuesForMax)
+        randValue = random.uniform(0, total)
+        s = valuesForMax[0]
+        for i in range(len(valuesForMax)):
+            if randValue <= s: # The one with index i is chosen
+                break
+            else:
+                s += valuesForMax[i+1]
+        return neighbors[i], valuesForMin[i]
+
+    def initTemp(self, p): # To set initial acceptance probability to 0.5
+        diffs = []
+        for i in range(self._numSample):
+            c0 = p.randomInit()     # A random point
+            v0 = p.evaluate(c0)     # Its value
+            c1 = p.randomMutant(c0) # A mutant
+            v1 = p.evaluate(c1)     # Its value
+            diffs.append(abs(v1 - v0))
+        dE = sum(diffs) / self._numSample  # Average value difference
+        t = dE / math.log(2)        # exp(–dE/t) = 0.5
+        return t
+
+    def tSchedule(self, t):
+        return t * (1 - (1 / 10**4))
---
+++ HW08/problem.py
@@ -0,0 +1,295 @@
+from setup import Setup
+import random
+import math
+
+DELTA = 0.01 
+
+class Problem(Setup):
+    def __init__(self):
+        Setup.__init__(self)
+        self._solution = []
+        self._value = 0
+        self._numEval = 0
+
+    def setVariables(self): # interface만 제공
+        pass
+    
+    def randomInit(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def mutants(self):
+        pass
+
+    def randomMutant(self, current):
+        pass
+
+    def describe(self):
+        pass
+
+    def storeResult(self, solution, value):
+        self._solution = solution
+        self._value = value
+
+    def report(self):
+        print()
+        print("Total number of evaluations: {0:,}".format(self._numEval))
+
+    def getSolution(self):
+        return self._solution
+    
+    def getValue(self):
+        return self._value
+
+    def getNumEval(self):
+        return self._numEval
+
+
+class Numeric(Problem):
+    def __init__(self):
+        Problem.__init__(self)
+        # super().__init__(self) 둘다 가능
+        self._expression = ''
+        self._domain = []     # domain as a list
+
+    def setVariables(self):
+        fileName = input("Enter the file name of a function: ")
+        infile = open(fileName, 'r')
+        self._expression = infile.readline()
+        varNames = []
+        low = []
+        up = []
+        line = infile.readline()
+        while line != '':
+            data = line.split(',')
+            varNames.append(data[0])
+            low.append(float(data[1]))
+            up.append(float(data[2]))
+            line = infile.readline()
+        infile.close()
+        self._domain = [varNames,low,up]
+
+    def getDelta(self): # 델타 리턴
+        return self._delta
+    
+    def getAlpha(self): # 알파 리턴
+        return self._alpha
+
+    def getDx(self):    # dx 리턴
+        return self._dx
+
+    def takeStep(self, x, v):   # 기울기의 음의 방향으로 이동
+        grad = self.gradient(x, v)  # 현재 위치에서의 기울기 계산
+        xCopy = x[:] # 복사
+
+        for i in range(len(xCopy)): # x길이 만큼 반복
+            xCopy[i] -= self._alpha * grad[i]   # 기울기 음의 방향으로 이동 
+        
+        if self.isLegal(xCopy):  # 범위가 타당한지 확인 하기 
+            return xCopy
+        else:
+            return x
+
+    def gradient(self,x,v): # 해당부분에서의 기울기를 구한다.
+        grad = []
+
+        for i in range(len(x)):
+            xCopyH = x[:]   # 복사
+            xCopyH[i] += self._dx # x_에 dx를 더해준다. !
+            g = (self.evaluate(xCopyH)-v)/self._dx  # 기울기 계산
+            grad.append(g)  # 기울기 리스트에 추가 
+        return grad # 리스트 반환
+
+    def isLegal(self, x):   # low, up 추출
+        domain = self._domain
+        low = domain[1]
+        up = domain[2]
+        flag = True
+
+        for i in range(len(low)): # x길이 만큼 돌면서 범위내에 있는지 확인
+            if x[i] < low[i] or x[i] > up[i]:
+                flag = False    # 범위 벗어나면?
+                break
+        return flag
+
+    def randomInit(self):
+        domain = self._domain   # domain: [varNams, low, up]
+        low, up = domain[1], domain[2]
+        init= []
+        for i in range(len(low)):   # For each variable
+            r= random.uniform(low[i],up[i]) # take a random value
+            init.append(r)
+        return init
+
+
+       
+    def describe(self):
+        print()
+        print("Objective function:")
+        print(self._expression)   # Expression
+        print("Search space:")
+        varNames = self._domain[0]
+        low = self._domain[1]
+        up = self._domain[2]
+        for i in range(len(low)):
+            print(" "+varNames[i]+":",(low[i],up[i]))
+
+    def report(self):
+        print()
+        print("Solution found:")
+        print(self.coordinate())  # Convert list to tuple
+        print("Minimum value: {0:,.3f}".format(self._value))
+        Problem.report(self)
+
+    def coordinate(self):
+        c = [round(value,3) for value in self._solution]
+        return tuple(c)
+    
+    def evaluate(self,current):
+        self._numEval +=1
+        expr = self._expression
+        varNames = self._domain[0]
+        for i in range(len(varNames)):
+            assignment = varNames[i] + '=' + str(current[i])
+            exec(assignment)
+        return eval(expr) 
+    
+    def mutate(self, current,i,d):
+        mutant = current[:]
+        domain = self._domain        # [VarNames, low, up]
+        l = domain[1][i]     # Lower bound of i-th
+        u = domain[2][i]     # Upper bound of i-th
+        if l <= (mutant[i] + d) <= u:
+            mutant[i] += d
+        return mutant
+    
+    def mutants(self, current):
+        neighbors =[]
+        for i in range(len(current)):
+            mutant = self.mutate(current,i, self._delta)
+            neighbors.append(mutant)
+            mutant = self.mutate(current,i,-self._delta)
+            neighbors.append(mutant)
+        return neighbors
+    
+    def randomMutant(self,current): ### TODO  # 공통인 함수는 아님.
+        i = random.randint(0,len(current)-1)
+        if random.uniform(0,1)>0.5: # 0.5 확률로 +,- 결정
+            d = self.getDelta()    
+        else:
+            d = -self.getDelta()
+        return self.mutate(current, i, d)  # +,- 방향으로 mutate 시킴. (객체 메서드 호출)
+
+class Tsp(Problem):
+    def __init__(self):
+        Problem.__init__(self)
+        self._numCities = 0
+        self._locations = []       # A list of tuples
+        self._distanceTable = []
+
+    ### Tsp methods
+    # tsp의 createProblem을 setVariables 메소드로 변경
+    def setVariables(self):
+        fileName = input("Enter the file name of a TSP: ")
+        infile = open(fileName, 'r')  # 파일 경로 입력받기
+        self._numCities = int(infile.readline())
+        locations = []
+        line = infile.readline()  # The rest of the lines are locations
+        while line != '':
+            locations.append(eval(line))  # 튜플 만들고 추가하고 읽고 닫기
+            line = infile.readline()
+        infile.close()
+        self._locations = locations # 읽은 locations을 self._locations에 넣기
+        self._distanceTable = self.calcDistanceTable()  # table 대신 self._distanceTable로 두기
+
+
+    def calcDistanceTable(self):
+        table = [[0] * self._numCities for _ in range(self._numCities)]
+
+        for i in range(self._numCities):
+            for j in range(self._numCities):
+                if i != j:
+                    distance = math.sqrt((self._locations[j][0] - self._locations[i][0]) ** 2 + (self._locations[j][1] - self._locations[i][1]) ** 2)
+                    table[i][j] = distance
+
+        return table
+
+    def randomInit(self):
+        n =self._numCities # p[0]은 numcitiest
+        init = list(range(n))
+        random.shuffle(init)
+        return init
+
+    def evaluate(self, current):
+        self._numEval += 1  # Problem에있는 _numEval을 ++ 해주며 얼마나 많이 evalution 하는지 추척해준다.
+
+        n = self._numCities    # p[0] 은 numcities
+        table = self._distanceTable    # p[2] = distance table 임
+        cost = 0    # 0에서부터 for 문을 돌면서 n-1만큼 sum 진행
+
+        for i in range(n-1):
+            locFrom = current[i]
+            locTo = current[i+1]
+            cost += table[locFrom][locTo]
+
+        cost += table[current[-1]][current[0]]
+        return cost
+    
+    def mutants(self, current):
+        n = self._numCities
+        neighbors = []
+        count = 0
+        triedPairs = []
+        while count <= n:  # Pick two random loci for inversion
+            i, j = sorted([random.randrange(n) for _ in range(2)])
+            if i < j and [i, j] not in triedPairs:
+                triedPairs.append([i, j])
+                curCopy = self.inversion(current, i, j)
+                count += 1
+                neighbors.append(curCopy)
+        return neighbors
+
+    def inversion(self, current, i, j): # 파라미터만 바꿔준다.
+        curCopy = current[:]
+        while i < j:
+            curCopy[i], curCopy[j] = curCopy[j], curCopy[i]
+            i += 1
+            j -= 1
+        return curCopy
+
+    def randomMutant(self,current): # Apply inversion
+        while True:
+            i, j = sorted([random.randrange(self._numCities)
+                        for _ in range(2)])
+            if i < j:
+                curCopy = self.inversion(current, i, j)
+                break
+        return curCopy
+
+
+    # describeProblem
+    def describe(self):
+        print()
+        n = self._numCities # p[0] 은 self._numCities니까 변경하기
+        print("Number of cities:", n)
+        print("City locations:")
+        locations = self._locations # p[1] 은 self._locations니까 변경하기
+        for i in range(n):
+            print("{0:>12}".format(str(locations[i])), end = '')
+            if i % 5 == 4:
+                print()
+
+    def tenPerRow(self):   # 그대로 
+        for i in range(len(self._solution)):
+            print("{0:>5}".format(self._solution[i]), end='')
+            if i % 10 == 9:
+                print()
+
+    def report(self):
+        print()
+        print("Best order of visits:")
+        self.tenPerRow()
+        print("Minimum tour cost: {0:,}".format(round(self._value)))   
+        super().report()
